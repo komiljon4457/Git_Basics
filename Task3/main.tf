a557e012df8af3aaa6ae28231fe8e770ce696438
Justin_Infinite <72872029+komiljon4457@users.noreply.github.com>
2022-02-28 17:32:06 +0500

    Update main.tf

--- Task3/main.tf
+++ Task3/main.tf
@@ -62,41 +62,22 @@ resource "aws_security_group" "Ubuntu" {
   }
 
 }
-# Linux 2 AMI image
-resource "aws_instance" "Linux" {
-  ami                    = "ami-055d15d9cfddf7bd3"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.Linux.id]
-
-
-  tags = {
-    Name = "Second Ubuntu server"
+# Redhat Enterprice 2 AMI image
+data "aws_ami" "redhat-linux-8" {
+  most_recent = true
+  owners      = ["309956199498"]
+  filter {
+    name   = "name"
+    values = ["RHEL-8.*"]
   }
 }
-resource "aws_security_group" "Linux" {
-  name = "Linux"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["172.31.28.107/32"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["172.31.28.107/32"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["172.31.28.107/32"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["172.31.28.107/32"]
+resource "aws_instance" "rhel-server" {
+  ami                    = data.aws_ami.redhat-linux-8.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.SecurityGroup.id]
+  subnet_id              = aws_subnet.private-subnet.id
+  key_name = "accesser"
+  tags = {
+    Name = var.rhel_name
   }
 }
