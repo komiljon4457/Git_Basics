dd8b7b257580c9a08cdefc290cbb16e105a4f32e
Justin_Infinite <72872029+komiljon4457@users.noreply.github.com>
2022-02-28 18:07:14 +0500

    Create network.tf

---
+++ Task3/network.tf
@@ -0,0 +1,27 @@
+resource "aws_vpc" "exadel-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "vpc"
+  }
+}
+
+
+// Public Subnet
+resource "aws_subnet" "public-subnet" {
+  vpc_id = aws_vpc.exadel-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+// Private Subnet
+resource "aws_subnet" "private-subnet" {
+  vpc_id = aws_vpc.exadel-vpc.id
+  cidr_block = "10.0.10.0/24"
+  tags = {
+    Name = "Public Subnet"
+  }
+}
